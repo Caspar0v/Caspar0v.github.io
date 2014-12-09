1df32cb608ee45f424bca17dff8f245f2cda9c36
Caspar0v <cbvanbeusekom@12move.nl>
2014-12-09 14:25:55 +0100

    bump

--- main.js
+++ main.js
@@ -94,7 +94,7 @@ function saveGame(how) {
 	console.log("Save local");
 	}
 	else if(how=="auto"){
-		console.log(auto);
+		console.log(autoSave);
 		if(autoSave){
 			autoSave = false;
 			if(typeof autoSaveInterval !== "undefined")clearInterval(autoSaveInterval);
