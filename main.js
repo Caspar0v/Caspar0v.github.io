0ab83449c1711f6c5e079b7ed5e37b6d8720f659
Caspar0v <cbvanbeusekom@12move.nl>
2014-12-09 14:56:49 +0100

    bump

--- main.js
+++ main.js
@@ -99,7 +99,7 @@ function saveGame(how) {
 		if(autoSave){
 			autoSave = false;
 			if(typeof autoSaveInterval !== "undefined")clearInterval(autoSaveInterval);
-			autoState = !autoSave;
+			autoState = "False";
 			document.getElementById("autoState").innerHTML = autoState;
 		}
 		else{
@@ -112,7 +112,7 @@ function saveGame(how) {
 					saveGame("local");
 				}
 			},1000);
-			autoState = !autoSave;
+			autoState = "True";
 			document.getElementById("autoState").innerHTML = autoState;
 		}
 	}
