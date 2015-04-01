fa43708f24e9e4133840c997daa244cdf378fb3f
Caspar0v <cbvanbeusekom@12move.nl>
2015-04-01 16:56:04 +0200

    boem

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
 //To make sure your screens won't overlap at start page;
-
-
-
 function tbo(){
 	document.getElementById('begin').innerHTML = "";
 }
 
-
-
-var coin= {
+var coin = {
 		amount: 0,
 		mp: 1
 };
 
 
-var building= {
+var building = {
 		'0': 0, //paperround
 		'1': 0, //constore
 		'2': 0, //carwash
@@ -22,39 +17,40 @@ var building= {
 		'4': 0 //motel
 };
 
-var price= {
+var price = {
 		'0': 10 //paperround
 }
 
-var level= {
+var level = {
 		level: 1,
 		abilitypoints: 1,
 		xp: 0,
 		xpNext: 127
 };
 
-var ability= {
+var ability = {
 		constore: false,
 		carwash: false,
 		badbroker: false,
 		motel: false
 };
 
-$(coinplus).click(function coinClick(number) {
-	alert(daosjdoasjld);
-	coin.amount += number * coin.mp;
-	$(coin).innerHTML = coin.amount;
+$('#coinplus').on('click', function () {
+	alert("ehadl");
+	coin.amount = coin.amount + 1;
+	$('#coin').innerHTML = coin.amount;
 });
 
+$(document).ready(function (){
+	coin.amount = 1;
+	$('#coin').innerHTML = coin.amount;
+});
 
-
-/*function buyBuilding(number, what){
-	console.log(price[what]);
-	console.log(building[what]);
+function buyBuilding(number, what){
 	if(coin.amount >= price[what]){
 		coin.amount = coin.amount - price[what];
 		building[what] = building[what] + 1;
-		coin.amount;
+		document.getElementById('coin').innerHTML = coin.amount;
 		document.getElementById('paper').innerHTML = building[what];
 	}
 	else
@@ -63,7 +59,7 @@ $(coinplus).click(function coinClick(number) {
 	}
 
 
-}*/
+};
 
 
 
